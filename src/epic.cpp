46dd8a4bb6f4c54c80b87271dff3c4be134c789a
Christian Bernt Haakonsen <c@uwc.net>
2012-01-17 18:33:52 -0500

    Created Autotools C++ Hello World project with Eclipse.

---
+++ src/epic.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : epic.cpp
+ Author      : Christian Bernt Haakonsen
+ Version     :
+ Copyright   : Copyright
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
