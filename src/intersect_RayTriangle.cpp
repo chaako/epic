048eb09708cc44db765a266b3dd0c5dabfa3d509
Christian Bernt Haakonsen <c@uwc.net>
2012-03-24 02:30:58 -0400

    Added accidentally omitted ray-tracing file.

---
+++ src/intersect_RayTriangle.cpp
@@ -0,0 +1,104 @@
+/*
+ * intersect_RayTriangle.cpp
+ *
+ *  Created on: Mar 19, 2012
+ *      Author: chaako
+ */
+
+// Copyright 2001, softSurfer (www.softsurfer.com)
+// This code may be freely used and modified for any purpose
+// providing that this copyright notice is included with it.
+// SoftSurfer makes no warranty for this code, and cannot be held
+// liable for any real or imagined damage resulting from its use.
+// Users of this code must verify correctness for their application.
+
+// Assume that classes are already given for the objects:
+//    Point and Vector with
+//        coordinates {float x, y, z;}
+//        operators for:
+//            == to test equality
+//            != to test inequality
+//            (Vector)0 = (0,0,0)         (null vector)
+//            Point  = Point ± Vector
+//            Vector = Point - Point
+//            Vector = Scalar * Vector    (scalar product)
+//            Vector = Vector * Vector    (cross product)
+//    Line and Ray and Segment with defining points {Point P0, P1;}
+//        (a Line is infinite, Rays and Segments start at P0)
+//        (a Ray extends beyond P1, but a Segment ends at P1)
+//    Plane with a point and a normal {Point V0; Vector n;}
+//    Triangle with defining vertices {Point V0, V1, V2;}
+//    Polyline and Polygon with n vertices {int n; Point *V;}
+//        (a Polygon has V[n]=V[0])
+//===================================================================
+
+#include "epic.h"
+
+#define SMALL_NUM  0.00000001 // anything that avoids division overflow
+// dot product (3D) which allows vector operations in arguments
+//#define dot(u,v)   ((u).x * (v).x + (u).y * (v).y + (u).z * (v).z)
+
+// intersect_RayTriangle(): intersect a ray with a 3D triangle
+//    Input:  a ray R, and a triangle T
+//    Output: *I = intersection point (when it exists)
+//    Return: -1 = triangle is degenerate (a segment or point)
+//             0 = disjoint (no intersect)
+//             1 = intersect in unique point I1
+//             2 = are in the same plane
+int
+intersect_RayTriangle( std::vector<Eigen::Vector3d> R,
+		std::vector<Eigen::Vector3d> T, Eigen::Vector3d* I )
+{
+    Eigen::Vector3d    u, v, n;             // triangle vectors
+    Eigen::Vector3d    dir, w0, w;          // ray vectors
+    float     r, a, b;             // params to calc ray-plane intersect
+
+    // get triangle edge vectors and plane normal
+    u = T[1] - T[0];
+    v = T[2] - T[0];
+    n = u.cross(v);             // cross product
+    if (n == Eigen::Vector3d(0.,0.,0.))            // triangle is degenerate
+        return -1;                 // do not deal with this case
+
+    dir = R[1] - R[0];             // ray direction vector
+    w0 = R[0] - T[0];
+    a = -n.dot(w0);
+    b = n.dot(dir);
+    if (fabs(b) < SMALL_NUM) {     // ray is parallel to triangle plane
+        if (a == 0)                // ray lies in triangle plane
+            return 2;
+        else return 0;             // ray disjoint from plane
+    }
+
+    // get intersect point of ray with triangle plane
+    r = a / b;
+    if (r < 0.0)                   // ray goes away from triangle
+        return 0;                  // => no intersect
+    // for a segment, also test if (r > 1.0) => no intersect
+
+    *I = R[0] + r * dir;           // intersect point of ray and plane
+
+    // is I inside T?
+    float    uu, uv, vv, wu, wv, D;
+    uu = u.dot(u);
+    uv = u.dot(v);
+    vv = v.dot(v);
+    w = *I - T[0];
+    wu = w.dot(u);
+    wv = w.dot(v);
+    D = uv * uv - uu * vv;
+
+    // get and test parametric coords
+    float s, t;
+    s = (uv * wv - vv * wu) / D;
+    if (s < 0.0 || s > 1.0)        // I is outside T
+        return 0;
+    t = (uv * wu - uu * wv) / D;
+    if (t < 0.0 || (s + t) > 1.0)  // I is outside T
+        return 0;
+
+    return 1;                      // I is in T
+}
+
+
+
